42d6912e2b8a35844ca1f52104a3914ebf60bf5d
Abouzar Kamaee <abouzarkamaee@gmail.com>
2015-02-03 16:18:21 +0330

    Add Main File To Hoopad

---
+++ Manifest/UI/Steps/Hoopad/Main.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Manifest.UI.Steps.Hoopad
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : System.Windows.Controls.UserControl
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
