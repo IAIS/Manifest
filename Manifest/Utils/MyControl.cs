f7a046b3e1e1d3fd4e5746a34f92ad20218a4aa4
Abouzar Kamaee <abouzarkamaee@gmail.com>
2015-02-03 17:20:57 +0330

    Final Changes

--- Manifest/Utils/MyControl.cs
+++ Manifest/Utils/MyControl.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using FirstFloor.ModernUI.Windows;
 using FirstFloor.ModernUI.Windows.Navigation;
 using Warehouse.Exceptions;
