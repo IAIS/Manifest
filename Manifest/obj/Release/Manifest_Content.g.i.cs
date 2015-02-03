6422a6467147164bfb657c8e5e9a16adcb722073
Abouzar Kamaee <abouzarkamaee@gmail.com>
2015-02-03 16:05:14 +0330

    Deploy Files Has Been Added.

--- Manifest/obj/Release/Manifest_Content.g.i.cs
+++ Manifest/obj/Release/Manifest_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("deploy/setup.exe")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("structure/map.xml")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("structure/templates.xml")]
 
